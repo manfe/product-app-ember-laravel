5dd75ff857f9d10013677fb66509c9b569fe2180
Mauricio N. Ferreir (manfe) <contato@ferreiramauricio.com>
2017-11-14 00:20:00 -0200

    Added Fractal to generate json in JSON:API spec

--- laravel-app/app/Http/Controllers/ProductController.php
+++ laravel-app/app/Http/Controllers/ProductController.php
@@ -5,9 +5,28 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product as Product;
 
+use League\Fractal\Manager as Manager;
+use League\Fractal\Resource\Collection;
+
+use App\Transformers\ProductTransformer as ProductTransformer;
+
 class ProductController extends Controller
 {
-    public function index() {
-        return Product::all()->toJson();
+    private $fractal;
+    private $productTransformer;
+
+    function __construct(Manager $fractal, ProductTransformer $productTransformer)
+    {
+        $this->fractal = $fractal;
+        $this->productTransformer = $productTransformer;
     }
-}
+
+    public function index(Request $request)
+    {
+        $products = Product::all(); // Get users from DB
+        $products = new Collection($products, $this->productTransformer); // Create a resource collection transformer
+        $products = $this->fractal->createData($products); // Transform data
+
+        return $products->toArray(); // Get transformed array of data
+    }
+}    
